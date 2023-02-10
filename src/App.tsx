f01f369b2786f6c136a6584505a2856f31502984
Jemima <contatojemima@gmail.com>
2023-02-10 15:24:10 -0300

    small styles adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { defaultTheme } from './styles/themes'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,9 +13,11 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    transition: all .1s ease-out;
   }
 
   body {
+    height: 100vh;
     /* font-family: t26-carbon, monospace; */
     font-family: 'Khula', sans-serif;
     font-weight: 700;
